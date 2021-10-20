e3db0a28e1a4f6a34faf15eb977991acab40be21
Samonse <58351692+Samonse@users.noreply.github.com>
2021-10-20 13:33:45 -0500

    Add files via upload

---
+++ prueba.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 17-10-2021 a las 04:04:09
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 8.0.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `prueba`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `datos`
+--
+
+CREATE TABLE `datos` (
+  `nombre` varchar(155) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `email` varchar(155) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `mensaje` varchar(500) COLLATE utf8mb4_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `datos`
+--
+
+INSERT INTO `datos` (`nombre`, `email`, `mensaje`) VALUES
+('Se', 'samonse_tssm@gmail.com', 'emdsp'),
+('Berta', 'tuska@hotmail.com', 'bertita');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
